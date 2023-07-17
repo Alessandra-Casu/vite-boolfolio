5f2f918d82a6de385bb1b1f8845561f056d656f2
Alessandra-Casu <alessandracasu919@gmail.com>
2023-07-17 14:36:09 +0200

    mileston 3

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      arrProjects: [],
+      nPage: 1,
+    };
+  },
+  created() {
+    //richiesta dati al server
+    axios
+      .get("http://127.0.0.1:8000/api/projects", {
+        params: {
+          page: this.nPage,
+        },
+      })
+      .then((response) => (this.arrProjects = response.data.data));
+  },
+};
+//this.arrProjects = response.data.data
+</script>
+
+<template>
+  <h2>questi sono i nostri progetti</h2>
+  <ul>
+    <li v-for="project in arrProjects" :key="project.id">
+      {{ project.title }}
+    </li>
+  </ul>
+</template>
+
+<style></style>
